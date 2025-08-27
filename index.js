78ac67fee14a61f8808eaf452c7c25b281612cd4
Joaquim <joaquimoliart123@gmail.com>
2025-08-27 20:54:23 -0300

    feat: delete

--- index.js
+++ index.js
@@ -26,9 +26,9 @@ app.post("/usuarios", (req, res) => {
     }
 
     const novoUsuario = {
+        id:ultimoId + 1,
         nome:nome,
-        email:email,
-        id:ultimoId + 1
+        email:email
     }
 
     usuarios.push(novoUsuario);
@@ -37,4 +37,37 @@ app.post("/usuarios", (req, res) => {
     res.status(201).json(novoUsuario.id);
 });
 
+app.delete("/usuarios/:id", (req, res) => {
+    const id = req.params.id
+    const idNumerico = parseInt(id);
+
+    if (isNaN(idNumerico)) {
+        return res
+        .status(400)
+        .json({ mensagem: "ID inválido, precisa ser um número" });
+    }
+
+    let posicao_do_usuario = usuarios.findIndex(
+        (usuario) => usuario.id === idNumerico
+    );
+
+    if(posicao_do_usuario === -1) {
+        return res.status(404).json({ mensagem: "Usuário não encontrado" });
+    }
+    //seguir daqui se eu nao tenho usuario
+
+    usuarios.splice(posicao_do_usuario, 1);
+        res.status(204).send();
+});
+
 app.listen(3000);
+
+/*
+DELETAR
+
+PRECISA DO ID DO USUÁRIO A SER DELETADO
+- TRANFORMAR O ID DE STRING PARA NÚMERO
+- SE O ID FOR INVALIDO, RETORNA 400
+- VERIFICAR SE O USUÁRIO EXISTE
+- SE NÃO EXISTIR, RETORNAR 404
+ */
